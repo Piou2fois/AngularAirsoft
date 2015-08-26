c70a01a6f83e834a56db90c6ae788c6ac2aea2aa
Piou2fois <guillaume.massart@gmail.com>
2015-08-27 01:17:14 +0200

    Add some properties for the players
    PLAYERS_BIRTHDATE, PLAYERS_ADDRESS,PLAYERS_PHONE

--- ajax/players_update.php
+++ ajax/players_update.php
@@ -7,16 +7,23 @@
       UPDATE
         T_PLAYERS
       SET
-        PLAYERS_LASTNAME=:lastname
-        ,PLAYERS_FIRSTNAME=:firstname
-        ,PLAYERS_NICKNAME=:nickname
-        ,PLAYERS_TEAM=:team
+        PLAYERS_LASTNAME=:PLAYERS_LASTNAME
+        ,PLAYERS_FIRSTNAME=:PLAYERS_FIRSTNAME
+        ,PLAYERS_NICKNAME=:PLAYERS_NICKNAME
+        ,PLAYERS_TEAM=:PLAYERS_TEAM
+        ,PLAYERS_BIRTHDATE=:PLAYERS_BIRTHDATE
+        ,PLAYERS_ADDRESS=:PLAYERS_ADDRESS
+        ,PLAYERS_PHONE=:PLAYERS_PHONE
       WHERE
-        PLAYERS_ID=:id');
-	$stmt->bindValue(':id',$request->PLAYERS_ID,PDO::PARAM_INT);
-  $stmt->bindValue(':lastname',$request->PLAYERS_LASTNAME,PDO::PARAM_STR);
-  $stmt->bindValue(':firstname',$request->PLAYERS_FIRSTNAME,PDO::PARAM_STR);
-  $stmt->bindValue(':nickname',$request->PLAYERS_NICKNAME,PDO::PARAM_STR);
-  $stmt->bindValue(':team',$request->PLAYERS_TEAM,PDO::PARAM_STR);
+        PLAYERS_ID=:PLAYERS_ID');
+	$stmt->bindValue(':PLAYERS_ID',$request->PLAYERS_ID,PDO::PARAM_INT);
+  $stmt->bindValue(':PLAYERS_LASTNAME',$request->PLAYERS_LASTNAME,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_FIRSTNAME',$request->PLAYERS_FIRSTNAME,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_NICKNAME',$request->PLAYERS_NICKNAME,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_TEAM',$request->PLAYERS_TEAM,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_BIRTHDATE',$request->PLAYERS_BIRTHDATE,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_ADDRESS',$request->PLAYERS_ADDRESS,PDO::PARAM_STR);
+  $stmt->bindValue(':PLAYERS_PHONE',$request->PLAYERS_PHONE,PDO::PARAM_STR);
 	$stmt->execute();
+  print_r($request);
 ?>
